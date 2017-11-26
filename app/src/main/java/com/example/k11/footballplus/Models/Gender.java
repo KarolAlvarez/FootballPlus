546dbe4fb0b3fe2014e83d225b3fd3611b3178f9
KarolAlvarez <aalvarez3570@gmail.com>
2017-11-26 01:06:45 -0500

    listar registro de usuario

---
+++ app/src/main/java/com/example/k11/footballplus/Models/Gender.java
@@ -0,0 +1,11 @@
+package com.example.k11.footballplus.Models;
+
+/**
+ * Created by k11 on 25/11/17.
+ */
+
+public class Gender {
+
+    public static final String MALE = "http://www.mountheavenspa.com//wp-content/uploads/2017/03/male-100x100.png";
+    public static final String FEMALE = "http://www.clker.com/cliparts/b/d/F/4/j/j/female-professional-th.png";
+}
--- app/src/main/java/com/example/k11/footballplus/Views/CreateUserActivity.java
+++ app/src/main/java/com/example/k11/footballplus/Views/CreateUserActivity.java
@@ -1,29 +1,117 @@
 package com.example.k11.footballplus.Views;
 
+import android.content.ContentValues;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.support.design.widget.TextInputEditText;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
+import com.example.k11.footballplus.Helpers.SqliteHelper;
 import com.example.k11.footballplus.LoginActivity;
+import com.example.k11.footballplus.Models.Gender;
 import com.example.k11.footballplus.R;
+import com.example.k11.footballplus.Utilities.Constants;
 
 public class CreateUserActivity extends AppCompatActivity {
-private Button btnCreateUser;
+    private Button btnCreateUser;
+    private String flagImgUser, flagGender;
+    TextInputEditText edtNameCreateUser, edtLastNameCreateUser, edtPasswordCreateUser, edtUserNameCreateUser;
+    RadioGroup radioGroupButonCreateuser;
+    SqliteHelper sqliteHelper;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_user);
-        btnCreateUser=(Button)findViewById(R.id.btnCreateUser);
+
+
+        edtNameCreateUser = (TextInputEditText) findViewById(R.id.edtNameCreateUser);
+        edtLastNameCreateUser = (TextInputEditText) findViewById(R.id.edtLastNameCreateUser);
+        edtPasswordCreateUser = (TextInputEditText) findViewById(R.id.edtPasswordCreateUser);
+        edtUserNameCreateUser = (TextInputEditText) findViewById(R.id.edtUserNameCreateUser);
+        radioGroupButonCreateuser = (RadioGroup) findViewById(R.id.radioGroupButonCreateuser);
+
+        sqliteHelper = new SqliteHelper(this, "DB_CAMP_FOOTBALL", null, 1);
+
+
+        radioGroupButonCreateuser.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int checkedId) {
+                if (checkedId == R.id.radioButtonMaleCreateUser) {
+                    flagImgUser = Gender.MALE;
+                    flagGender = "Man";
+                } else if (checkedId == R.id.radioButtonFemaleCreateUser) {
+                    flagImgUser = Gender.FEMALE;
+                    flagGender = "Woman";
+                }
+            }
+        });
+
+
+        btnCreateUser = (Button) findViewById(R.id.btnCreateUser);
         btnCreateUser.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent=new Intent(view.getContext(), LoginActivity.class);
-                startActivity(intent);
+
+                onClickcreateUser();
+
             }
         });
     }
+
+
+    public void onClickcreateUser() {
+
+
+        String stringName = edtNameCreateUser.getText().toString();
+        String stringLastName = edtLastNameCreateUser.getText().toString();
+        String stringUserName = edtUserNameCreateUser.getText().toString();
+        String stringPassword = edtPasswordCreateUser.getText().toString();
+
+
+        if (TextUtils.isEmpty(stringName)) {
+            Toast.makeText(this, "The name field is empty", Toast.LENGTH_SHORT).show();
+        } else if (TextUtils.isEmpty(stringLastName)) {
+            Toast.makeText(this, "The lastname field is empty", Toast.LENGTH_SHORT).show();
+        } else if (TextUtils.isEmpty(stringUserName)) {
+            Toast.makeText(this, "The username field is empty", Toast.LENGTH_SHORT).show();
+        } else if (TextUtils.isEmpty(stringPassword)) {
+            Toast.makeText(this, "The password field is empty", Toast.LENGTH_SHORT).show();
+        } else if (TextUtils.isEmpty(flagImgUser)) {
+            Toast.makeText(this, "the gender has not been selected", Toast.LENGTH_SHORT).show();
+        } else {
+            createUser();
+        }
+    }
+
+    public void createUser() {
+
+        SQLiteDatabase db = sqliteHelper.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put(Constants.TABLA_USER_NAME, edtNameCreateUser.getText().toString());
+        values.put(Constants.TABLA_USER_LASTNAME, edtLastNameCreateUser.getText().toString());
+        values.put(Constants.TABLA_USER_USERNAME, edtUserNameCreateUser.getText().toString());
+        values.put(Constants.TABLA_USER_PIN, Integer.parseInt(edtPasswordCreateUser.getText().toString()));
+        values.put(Constants.TABLA_USER_GENDER, flagGender);
+        values.put(Constants.TABLA_USER_IMAGE, flagImgUser);
+
+        Long idResult = db.insert(Constants.TABLA_NAME_USER, Constants.TABLA_USER_ID, values);
+        edtNameCreateUser.setText("");
+        edtLastNameCreateUser.setText("");
+        edtPasswordCreateUser.setText("");
+        edtUserNameCreateUser.setText("");
+
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+    }
+
+
 }
