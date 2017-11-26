2696807bfa12ef8f25cf4d299317b237b6f77060
KarolAlvarez <aalvarez3570@gmail.com>
2017-11-25 20:51:55 -0500

    constantes bd

---
+++ app/src/main/java/com/example/k11/footballplus/Models/Users.java
@@ -0,0 +1,66 @@
+package com.example.k11.footballplus.Models;
+
+/**
+ * Created by k11 on 25/11/17.
+ */
+
+public class Users {
+    private Integer id,pin;
+    private String name,lastName,userName,gender,image;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getPin() {
+        return pin;
+    }
+
+    public void setPin(Integer pin) {
+        this.pin = pin;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+}
