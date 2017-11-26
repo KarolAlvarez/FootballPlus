06ce38ff1f6b4d04831942a27f8eef9aa04cc5fe
KarolAlvarez <aalvarez3570@gmail.com>
2017-11-25 20:54:46 -0500

    constantes bd

---
+++ app/src/main/java/com/example/k11/footballplus/Models/CampFootball.java
@@ -0,0 +1,59 @@
+package com.example.k11.footballplus.Models;
+
+/**
+ * Created by k11 on 25/11/17.
+ */
+
+public class CampFootball {
+
+    private Integer id,phone;
+    private String name,description,address,image;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getPhone() {
+        return phone;
+    }
+
+    public void setPhone(Integer phone) {
+        this.phone = phone;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+}
